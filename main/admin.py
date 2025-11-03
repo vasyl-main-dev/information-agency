e8d4bf6cf296c8c384d51b6f01888bcbedb54ed1
Vasyl <jomajok7777@gmail.com>
2025-11-03 23:29:48 +0200

    Implement and register Redactor, Topic and Newspaper models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from main.models import Newspaper, Redactor, Topic
+
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    search_fields = ("title", "content")
+    list_filter = ("publishers", "topics")
+    list_display = ("title", "published_date")
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Redactor)
+class RedactorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("year_of_experience",)
+    search_fields = ("username", "first_name", "last_name")
+    ordering = ("-year_of_experience",)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Redactor(AbstractUser):
+    year_of_experience = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f"{self.username}: {self.first_name} {self.last_name}"
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    published_date = models.DateField(auto_now_add=True)
+    topics = models.ManyToManyField(Topic, related_name="newspapers")
+    publishers = models.ManyToManyField(Redactor, related_name="newspapers")
+
+    class Meta:
+        ordering = ["-published_date"]
+
+    def __str__(self):
+        return self.title
