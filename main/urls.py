1cd97198905dde973b91cf43b050d7b0fa83fb06
Vasyl <jomajok7777@gmail.com>
2025-11-04 22:00:12 +0200

    Implement redactor_list, redactor_detail and redactor_confirm_delete pages

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 
-from main.views import index
+from main.views import (
+    index,
+    RedactorListView,
+    RedactorDetailView,
+    RedactorDeleteView
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path("redactors/", RedactorListView.as_view(), name="redactor-list"),
+    path("redactor/<int:pk>/", RedactorDetailView.as_view(), name="redactor-detail"),
+    path("redactor/<int:pk>/delete/", RedactorDeleteView.as_view(), name="redactor-delete"),
 ]
 
 app_name = "main"
--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import generic
 
 from main.models import Redactor
 
@@ -7,3 +9,17 @@ def index(request):
     redactors = Redactor.objects.all()
     context = {"redactors": redactors}
     return render(request, "main/index.html", context)
+
+
+class RedactorListView(generic.ListView):
+    model = Redactor
+    paginate_by = 10
+
+
+class RedactorDetailView(generic.DetailView):
+    model = Redactor
+
+
+class RedactorDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Redactor
+    success_url = "main:redactor-list"
