853999801c775f3ee8ce1fb3733f82737e5fcf82
Vasyl <jomajok7777@gmail.com>
2025-11-17 10:59:53 +0200

    Hide SECRET_KEY

--- information_agency/settings.py
+++ information_agency/settings.py
@@ -1,4 +1,8 @@
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-nl@-c1wgx-!l8hgidh+^rli+px^v4mb(p%vh(%ywhs5+e&r1t6"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
